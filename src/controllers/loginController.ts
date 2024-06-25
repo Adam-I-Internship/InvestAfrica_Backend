cecde27d60e5ebb25afed2c18b08d5f9bd5cb906
Durgesh Tiwari <118502636+Durgesh2022@users.noreply.github.com>
2024-06-25 23:18:18 +0530

    Update loginController.ts

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -5,30 +5,41 @@ import { LoginType } from '../types/loginTypes';
 
 const prisma = new PrismaClient();
 
-const findAccountByEmail = async (email: string) => {
-  return await prisma.user.findUnique({
-    where: { email },
+enum AccountType {
+  User = 'User',
+  Company = 'Company',
+}
+
+const checkAccountType = async (data: LoginType) => {
+  const account = await prisma.user.findUnique({
+    where: { email: data.email },
   });
+  if (account) {
+    return account;
+  }
+  const account2 = await prisma.company.findUnique({
+    where: { email: data.email },
+  });
+  if (account2) {
+    return account2;
+  }
+  return null;
 };
 
 const loginAccount = async (req: Request, res: Response) => {
   try {
-    const { email, accountPassword }: LoginType = req.body;
-    const account = await findAccountByEmail(email);
-
-    if (!account) {
+    const data = { ...req.body };
+    const account = await checkAccountType(data);
+    console.log(account);
+    if (account == null) {
       return res.status(400).json({ message: 'Cannot find account' });
     }
 
-    const isPasswordValid = await bcrypt.compare(accountPassword, account.accountPassword);
-
-    if (isPasswordValid) {
+    if (await bcrypt.compare(data.accountPassword, account.accountPassword)) {
       return res.status(200).json({ message: 'Account authenticated.' });
-    } else {
-      return res.status(401).json({ message: 'Account cannot be authenticated.' });
     }
+    return res.status(401).json({ message: 'Account cannot be authenticated.' });
   } catch (error) {
-    console.error('Login error:', error);
     return res.status(500).json({ error: 'Login Failed. Please try again later.' });
   }
 };
