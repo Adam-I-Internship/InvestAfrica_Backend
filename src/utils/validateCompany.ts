9312ccb660fe5e5ca28880ad3c02143c314fded4
Durgesh Tiwari <118502636+Durgesh2022@users.noreply.github.com>
2024-06-25 23:23:45 +0530

    Update validateCompany.ts

--- src/utils/validateCompany.ts
+++ src/utils/validateCompany.ts
@@ -1,35 +1,36 @@
 import Joi from 'joi';
-
+import { countries } from 'countries-list';
 import { CompanyType } from '../types/companyTypes';
 
-
+const validCountries = Object.values(countries).map(country => country.name);
 
 const companySchema = Joi.object({
-  country: Joi.string().max(255).required(),
-  city: Joi.string().max(255).required(),
-  postCode: Joi.string().max(255).allow(null).allow(''),
   companyName: Joi.string().max(255).required(),
   businessType: Joi.string().max(255).required(),
   numberOfEmployees: Joi.number().integer().strict().min(1).max(10000000).required(),
   yearOfEstablishment: Joi.number()
     .integer()
     .strict()
-    .min(1)
+    .min(1500)
     .max(new Date().getFullYear())
     .required(),
-    email: Joi.string()
-    .email({ tlds: { allow: false } })
-    .max(255)
+  country: Joi.string()
+    .valid(...validCountries)
     .required(),
-    phoneNumber: Joi.string()
-    .pattern(/^\+?[0-9-\s]*[0-9]{8,}[0-9-\s]*$/)
-    .required(),
+  city: Joi.string().max(255).required(),
+  postCode: Joi.string().max(255).allow(null).allow(''),
+  industry: Joi.string().max(255).required(),
   accountPassword: Joi.string()
     .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d$_.!@#%^&*()\-+=]{8,128}$/)
     .required(),
   confirmPassword: Joi.valid(Joi.ref('accountPassword')).required(),
-
-
+  phoneNumber: Joi.string()
+    .pattern(/^\+?[0-9-\s]*[0-9]{8,}[0-9-\s]*$/)
+    .required(),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .max(255)
+    .required(),
 }).with('accountPassword', 'confirmPassword');
 
 const validateCompanyData = (data: CompanyType) => {
