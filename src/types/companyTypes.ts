9816fc4faac5da26a41621acb13d58336ed30ed5
Durgesh Tiwari <118502636+Durgesh2022@users.noreply.github.com>
2024-06-25 21:43:57 +0530

    Update companyTypes.ts

--- src/types/companyTypes.ts
+++ src/types/companyTypes.ts
@@ -6,13 +6,11 @@ export type CompanyType = {
   country: string;
   city: string;
   postCode: string;
-  industry: string;
+  
   accountPassword: string;
   confirmPassword: string;
-  companyAddress: string;
-  personOfContact: string;
-  position: string;
+  
   phoneNumber: string;
   email: string;
-  companyWebsite: string;
+  
 };
